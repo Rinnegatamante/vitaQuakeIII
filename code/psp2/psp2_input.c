abecfd7a6a393867a4d5b8a09f8be88467e8e605
Rinnegatamante <rinnegatamante@gmail.com>
2018-03-15 13:18:39 +0100

    Renamed AUX keys to PSVITA ones.

--- code/psp2/psp2_input.c
+++ code/psp2/psp2_input.c
@@ -62,7 +62,7 @@ void Sys_SetKeys(uint32_t keys){
 	if((keys & SCE_CTRL_LTRIGGER) != (oldkeys & SCE_CTRL_LTRIGGER))
 		Key_Event(K_AUX5, (keys & SCE_CTRL_LTRIGGER) == SCE_CTRL_LTRIGGER, Sys_Milliseconds());
 	if((keys & SCE_CTRL_RTRIGGER) != (oldkeys & SCE_CTRL_RTRIGGER))
-		Key_Event(K_AUX7, (keys & SCE_CTRL_RTRIGGER) == SCE_CTRL_RTRIGGER, Sys_Milliseconds());
+		Key_Event(K_AUX6, (keys & SCE_CTRL_RTRIGGER) == SCE_CTRL_RTRIGGER, Sys_Milliseconds());
 }
 
 void IN_RescaleAnalog(int *x, int *y, int dead) {
